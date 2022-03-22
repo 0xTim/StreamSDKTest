051e3104f098a6571d7c3bb0edcf9155546dc2b2
Tim <0xtimc@gmail.com>
2022-03-22 11:59:33 +0000

    Initial Commit

---
+++ StreamTest/StreamTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamTestApp.swift
+//  StreamTest
+//
+//  Created by Tim Condon on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct StreamTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
